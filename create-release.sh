9d4f892c0b52e2e99143161c5eb27ea4cd979ea5
root <wudongfei@gmail.com>
2017-04-25 16:50:13 +0800

    hi

--- create-release.sh
+++ create-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 bosh2 create-release --tarball a.tgz
-tar -xOf ../a.tgz release.MF |sed -n -e "s/^version: //p" -e "s/^name: //p"|xargs|read name version;
+tar -xOf a.tgz release.MF |sed -n -e "s/^version: //p" -e "s/^name: //p"|xargs|read name version;
 mv a.tgz dev_releases/$name/${name}-${version}.tgz
