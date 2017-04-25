3ba94e089ed3a4aeb6a864f74036206fc72afd7f
root <wudongfei@gmail.com>
2017-04-25 15:52:23 +0800

    update

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git submodule update --init --recursive
-git submodule foreach git pull
+#git submodule update --init --recursive
+#git submodule foreach git pull
+mkdir src/go
+cd src/go
+wget https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
+cd -
